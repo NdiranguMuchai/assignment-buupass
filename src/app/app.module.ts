68e883b168a3ab9c482e8094e2c81116da3fa318
ndirangu <ndirangu.muchai@gmail.com>
2020-02-12 20:18:35 +0300

    video home page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeDetailsComponent } from './home-details/home-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HomeDetailsComponent
   ],
   imports: [
     BrowserModule,
